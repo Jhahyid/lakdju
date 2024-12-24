2b70b45630927eccd5fe9abd8020e0329e634011
Jhahyid <aeybdbsgtapdi+aidj6hs@gmail.com>
2024-12-24 01:42:23 -0500

    Create n.sh

---
+++ n.sh
@@ -0,0 +1,2 @@
+cd && wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.6.8/SRBMiner-Multi-2-6-8-Linux.tar.gz && tar -xzvf SRBMiner-Multi-2-6-8-Linux.tar.gz && clear && cd SRBMiner-Multi-2-6-8
+./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool sg.vipor.net:5040 --wallet RCQo8HQTG89eugDsgutCZBxfgFLSF6HMwk --Worker NH1 --password x --cpu-threads x
